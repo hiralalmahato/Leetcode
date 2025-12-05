09c54dcc6cf6ef789f251bd7e4f094240ec2f1d9
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2025-12-05 15:55:31 +0530

    Add solution for Longest Common Prefix

--- LeetCode/Longest-Common-Prefix/Solution.java
+++ LeetCode/Longest-Common-Prefix/Solution.java
@@ -14,22 +14,5 @@
 14
 15        
 16    }
-17    public static void main(String[] args){
-18        Scanner sc = new Scanner(System.in);
-19         
-20        int n  = sc.nextInt();
-21        sc.nextLine();
-22
-23
-24        String[] strs = new String[n];
-25        for(int i = 0;i<n;i++){
-26            strs[i] = sc.nextLine();
-27        }
-28
-29        Solution obj = new Solution();
-30        String result = obj.longestCommonPrefix(strs);
-31
-32        System.out.println(result);
-33        sc.close();
-34    }
-35}
+17   
+18}
