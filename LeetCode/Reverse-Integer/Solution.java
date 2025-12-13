f561a732c35d746d20205cfca5edf3373eb2a745
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2025-12-13 09:05:59 +0530

    Add solution for Reverse Integer

---
+++ LeetCode/Reverse-Integer/Solution.java
@@ -0,0 +1,25 @@
+1class Solution {
+2    public int reverse(int x) {
+3        int rev  = 0;
+4         while(x>0 || x<0){
+5            int digit = x%10;
+6             
+7            
+8            
+9
+10            if(rev>Integer.MAX_VALUE/10 ||(rev == Integer.MAX_VALUE && digit > 7)){
+11                return 0;
+12            }
+13            if(rev<Integer.MIN_VALUE/10 ||(rev==Integer.MIN_VALUE && digit < -8)){
+14                return 0;
+15            }
+16            rev = rev * 10 +digit;
+17            x  =x/10;
+18            
+19
+20
+21         }
+22         return rev;
+23        
+24    }
+25}
