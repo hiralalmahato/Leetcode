1a387ca36a6ae5b9ad6c4a67d56033dd809868fc
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2025-12-27 09:15:23 +0530

    Add solution for Letter Combinations of a Phone Number

--- LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
+++ LeetCode/Letter-Combinations-of-a-Phone-Number/Solution.java
@@ -1,32 +1,37 @@
-1class Solution {
-2    List<String> result = new ArrayList<>();
-3
-4    String[] map = {
-5        "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
-6    };
-7    public List<String> letterCombinations(String digits) {
-8        if(digits.length()==0){
-9            return result;
-10        }
-11        backtrack(digits,0,new StringBuilder());
-12        return result;
-13        
-14    }
-15
-16    public void backtrack(String digits, int index, StringBuilder current){
-17        if(index == digits.length()){
-18            result.add(current.toString());
-19            return;
-20
-21        }
-22
-23        String letters = map[digits.charAt(index)-'0'];
-24
-25        for(char c: letters.toCharArray()){
-26            current.append(c);
-27            backtrack(digits,index + 1, current);
-28            current.deleteCharAt(current.length() -1 );
-29
-30        }
-31    }
-32}
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        
+        if (digits == null || digits.length() == 0) {
+            return res;
+        }
+        
+        Map<Character, String> digitToLetters = new HashMap<>();
+        digitToLetters.put('2', "abc");
+        digitToLetters.put('3', "def");
+        digitToLetters.put('4', "ghi");
+        digitToLetters.put('5', "jkl");
+        digitToLetters.put('6', "mno");
+        digitToLetters.put('7', "pqrs");
+        digitToLetters.put('8', "tuv");
+        digitToLetters.put('9', "wxyz");
+        
+        backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
+        
+        return res;        
+    }
+
+    private void backtrack(String digits, int idx, StringBuilder comb, List<String> res, Map<Character, String> digitToLetters) {
+        if (idx == digits.length()) {
+            res.add(comb.toString());
+            return;
+        }
+        
+        String letters = digitToLetters.get(digits.charAt(idx));
+        for (char letter : letters.toCharArray()) {
+            comb.append(letter);
+            backtrack(digits, idx + 1, comb, res, digitToLetters);
+            comb.deleteCharAt(comb.length() - 1);
+        }
+    }    
+}
