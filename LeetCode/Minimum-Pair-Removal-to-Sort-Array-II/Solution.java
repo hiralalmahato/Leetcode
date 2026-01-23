d82f8ec48ab0fa85800cc82461e7c96932b3cae6
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2026-01-23 20:07:35 +0530

    Add solution for Minimum Pair Removal to Sort Array II

---
+++ LeetCode/Minimum-Pair-Removal-to-Sort-Array-II/Solution.java
@@ -0,0 +1,11 @@
+You are given an integer array.
+
+The array is considered valid if it is non-decreasing.
+(nums[i] <= nums[i+1] for all i)
+
+You are allowed to perform the following operation:
+➤ Pick any adjacent pair and merge them into one element
+  with value = sum of the pair.
+
+Your task is to find the minimum number of such operations
+required to make the array non-decreasing.
