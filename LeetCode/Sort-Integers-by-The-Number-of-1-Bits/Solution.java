0d99a94f5fc9aa408183ba660c1c043082c0e2f5
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2026-02-25 19:48:13 +0530

    Add solution for Sort Integers by The Number of 1 Bits

--- LeetCode/Sort-Integers-by-The-Number-of-1-Bits/Solution.java
+++ LeetCode/Sort-Integers-by-The-Number-of-1-Bits/Solution.java
@@ -1,22 +1,22 @@
-class Solution {
-    public int[] sortByBits(int[] arr) {
-        java.util.TreeMap<Integer, java.util.PriorityQueue<Integer>> bitmap = new java.util.TreeMap<>();
-        
-        for (int val : arr) {
-            int bits = Integer.bitCount(val);
-            bitmap.putIfAbsent(bits, new java.util.PriorityQueue<>());
-            bitmap.get(bits).add(val);
-        }
-        
-        int[] ans = new int[arr.length];
-        int index = 0;
-        
-        for (int key : bitmap.keySet()) {
-            java.util.PriorityQueue<Integer> pq = bitmap.get(key);
-            while (!pq.isEmpty()) {
-                ans[index++] = pq.poll();
-            }
-        }
-        return ans;
-    }
-}
+1class Solution {
+2    public int[] sortByBits(int[] arr) {
+3        java.util.TreeMap<Integer, java.util.PriorityQueue<Integer>> bitmap = new java.util.TreeMap<>();
+4        
+5        for (int val : arr) {
+6            int bits = Integer.bitCount(val);
+7            bitmap.putIfAbsent(bits, new java.util.PriorityQueue<>());
+8            bitmap.get(bits).add(val);
+9        }
+10        
+11        int[] ans = new int[arr.length];
+12        int index = 0;
+13        
+14        for (int key : bitmap.keySet()) {
+15            java.util.PriorityQueue<Integer> pq = bitmap.get(key);
+16            while (!pq.isEmpty()) {
+17                ans[index++] = pq.poll();
+18            }
+19        }
+20        return ans;
+21    }
+22}
