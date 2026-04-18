170bb3db3f817c842e2fd8fb5c47ddd947e35abd
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2026-04-18 23:11:28 +0530

    Add solution for Mirror Distance of an Integer

---
+++ LeetCode/Mirror-Distance-of-an-Integer/Solution.java
@@ -0,0 +1,11 @@
+n = 10
+
+revnum(10):
+  10 / 10 != 0, proceed
+  s = "10"
+  reverse --> s = "01"
+  stoi("01") = 1   <-- leading zero handled automatically
+  return 1
+
+mirrorDistance(10):
+  abs(10 - 1) = 9 ✓
