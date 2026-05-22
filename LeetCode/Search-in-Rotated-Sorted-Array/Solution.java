8607228efc7e84fe76919e231c1f043e1c31b044
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2026-05-22 21:56:36 +0530

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
@@ -1,35 +1,39 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int low =0;
-        int high = nums.length-1;
+public:
+    int search(vector<int>& nums, int target) {
 
-        while(low<=high){
-            int mid  = (low  + high)/2;
+        int low = 0;
+        int high = nums.size() - 1;
 
-            if(nums[mid]==target){
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) {
                 return mid;
             }
-            
-            //left half sorted
-            if(nums[low]<=nums[mid]){
 
-                if(nums[low]<=target && target<=nums[mid]){
-                    high = mid -1;
-                }else{
+            // left half is sorted
+            if(nums[low] <= nums[mid]) {
+
+                if(nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
                     low = mid + 1;
                 }
-
             }
-            //right half sorted
-            else{
-                if(nums[mid]<=target && target<=nums[high]){
+            // right half is sorted
+            else {
+
+                if(nums[mid] < target && target <= nums[high]) {
                     low = mid + 1;
-                }else{
-                    high =mid -1;
+                }
+                else {
+                    high = mid - 1;
                 }
             }
         }
+
         return -1;
-        
     }
-}
+};
