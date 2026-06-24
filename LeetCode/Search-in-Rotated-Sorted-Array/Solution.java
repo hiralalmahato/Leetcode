c98a9a7865f0724c5de5a15bf6c56765caa106cd
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2026-06-24 11:35:43 +0530

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
@@ -1,39 +1,88 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-
-        int low = 0;
-        int high = nums.size() - 1;
-
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if(nums[mid] == target) {
-                return mid;
-            }
-
-            // left half is sorted
-            if(nums[low] <= nums[mid]) {
-
-                if(nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                }
-                else {
-                    low = mid + 1;
-                }
-            }
-            // right half is sorted
-            else {
-
-                if(nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                }
-                else {
-                    high = mid - 1;
-                }
-            }
-        }
-
-        return -1;
-    }
-};
+1class Solution {
+2    public int search(int[] nums, int target) {
+3
+4        int left = 0;
+5        int right  = nums.length -1;
+6
+7        while(left<=right){
+8            int mid = left + (right - left) /2;
+9
+10            if(nums[mid]==target){
+11                return mid;
+12            }
+13
+14            if(nums[left] <=nums[mid]){
+15                if(nums[left]<=target && target<nums[mid]){
+16                    right = mid-1;
+17                }
+18                else{
+19                    left = mid +1;
+20                }
+21
+22            }
+23
+24
+25            else{
+26                if(nums[right]>=nums[mid]){
+27                    if(nums[right]>=target && target>nums[mid]){
+28                        left = mid +1;
+29                    }else{
+30                        right = mid-1;
+31                    }
+32                }
+33            }
+34        }
+35        return -1;
+36        
+37
+38        
+39        
+40    }
+41}
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
