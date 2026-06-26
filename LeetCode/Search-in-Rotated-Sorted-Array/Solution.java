0686b8f9a25a646f7b0b603796a544447ce35eab
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2026-06-26 07:00:36 +0530

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.java
@@ -1,44 +1,44 @@
 1class Solution {
 2    public int search(int[] nums, int target) {
 3
-4        int left = 0;
-5        int right  = nums.length -1;
-6
-7        while(left<=right){
-8            int mid = left + (right - left) /2;
-9
-10            if(nums[mid]==target){
-11                return mid;
-12            }
-13
-14            if(nums[left] <=nums[mid]){
-15                if(nums[left]<=target && target<nums[mid]){
-16                    right = mid-1;
-17                }
-18                else{
-19                    left = mid +1;
-20                }
-21
-22            }
-23
-24
-25            else{
-26                if(nums[right]>=nums[mid]){
-27                    if(nums[right]>=target && target>nums[mid]){
-28                        left = mid +1;
-29                    }else{
-30                        right = mid-1;
-31                    }
-32                }
-33            }
-34        }
-35        return -1;
-36        
+4        
+5    int left = 0;
+6    int  right = nums.length-1;
+7
+8
+9    while(left<=right) {
+10        int  mid  = left +(right-left)/2;
+11
+12        if(nums[mid]==target){
+13            return mid;
+14        }
+15
+16        if(nums[left]<=nums[mid]){
+17            if(nums[left]<=target && target<nums[mid]){
+18                right = mid -1;
+19            }else{
+20                left = mid + 1;
+21            }
+22        }
+23        else{
+24            if(nums[right]>=target && target > nums[mid]){
+25                left = mid +1;
+26            }
+27            else{
+28                right = mid -1;
+29            }
+30        }
+31    }
+32    return -1;
+33    }
+34}
+35
+36
 37
-38        
-39        
-40    }
-41}
+38
+39
+40
+41
 42
 43
 44
@@ -70,19 +70,43 @@
 70
 71
 72
-73
-74
+73// int left = 0;
+74//         int right  = nums.length -1;
 75
-76
-77
+76//         while(left<=right){
+77//             int mid = left + (right - left) /2;
 78
-79
-80
-81
+79//             if(nums[mid]==target){
+80//                 return mid;
+81//             }
 82
-83
-84
-85
-86
-87
-88
+83//             if(nums[left] <=nums[mid]){
+84//                 if(nums[left]<=target && target<nums[mid]){
+85//                     right = mid-1;
+86//                 }
+87//                 else{
+88//                     left = mid +1;
+89//                 }
+90
+91//             }
+92
+93
+94//             else{
+95//                 if(nums[right]>=nums[mid]){
+96//                     if(nums[right]>=target && target>nums[mid]){
+97//                         left = mid +1;
+98//                     }else{
+99//                         right = mid-1;
+100//                     }
+101//                 }
+102//             }
+103//         }
+104//         return -1;
+105
+106
+107
+108
+109
+110
+111
+112
