b7ae9098a4c67b90d680472adeac745ee0b553ba
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2026-06-30 11:08:33 +0530

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.java
+++ LeetCode/Climbing-Stairs/Solution.java
@@ -1,16 +1,17 @@
-class Solution {
-    public int climbStairs(int n) {
-        if(n==1) return 1;
-        
-        if(n==2) return 2;
-
-        int[] a =  new int[n];
-        a[0]=1;
-        a[1]=2;
-
-        for(int i=2;i<n;i++){
-            a[i]=a[i-1]+a[i-2];
-        }
-        return a[n-1];
-    }
-}
+1class Solution {
+2    public int climbStairs(int n) {
+3        if(n==1) return 1;
+4        
+5        if(n==2) return 2;
+6
+7        int [] num = new int[n];
+8
+9        num[0] =1;
+10        num[1] =2;
+11
+12        for(int i=2;i<n;i++){
+13            num[i] = num[i-2] + num[i-1];
+14        }
+15        return num[n-1];
+16    }
+17}
