b61e83873407fc13ee8df211c44b2e4b0be744b0
Hiralal Mahato <149621648+hiralalmahato@users.noreply.github.com>
2026-07-19 18:47:15 +0530

    Add solution for Valid Palindrome

--- LeetCode/Valid-Palindrome/Solution.java
+++ LeetCode/Valid-Palindrome/Solution.java
@@ -1,29 +1,26 @@
 1class Solution {
 2    public boolean isPalindrome(String s) {
 3
-4        int left = 0;
-5        int right = s.length() - 1;
+4       int left = 0;
+5       int right = s.length()-1;
 6
-7        while (left < right) {
-8
-9            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-10                left++;
-11            }
-12
-13            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-14                right--;
-15            }
+7       while(left<right){
+8        while(left <right && !Character.isLetterOrDigit(s.charAt(left))){
+9            left++;
+10        }
+11
+12        while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+13            right--;
+14
+15        }
 16
-17            if (Character.toLowerCase(s.charAt(left)) !=
-18                Character.toLowerCase(s.charAt(right))) {
-19                return false;
-20            }
-21
-22            left++;
-23            right--;
-24        }
-25
-26        return true;
-27    }
-28}
-29
+17        if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+18            return false;
+19        }
+20        left++;
+21        right--;
+22       }
+23       return true;
+24    }
+25}
+26
